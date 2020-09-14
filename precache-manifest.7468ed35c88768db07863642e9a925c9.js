43dd41f50a00727d54fe154852ee1058262f7ee0
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-09-14 00:24:29 -0600

    Updates

---
+++ precache-manifest.7468ed35c88768db07863642e9a925c9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3df1a39a2d708c859db31af287788b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8e4b5eb2cd66fb652e5e",
+    "url": "/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e75fac0a4797acfabfa9",
+    "url": "/portfolio/static/css/main.25554110.chunk.css"
+  },
+  {
+    "revision": "8e4b5eb2cd66fb652e5e",
+    "url": "/portfolio/static/js/2.42544d5d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.42544d5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e75fac0a4797acfabfa9",
+    "url": "/portfolio/static/js/main.a263ff84.chunk.js"
+  },
+  {
+    "revision": "309a9e55c61ba5bff665",
+    "url": "/portfolio/static/js/runtime-main.b3df5f95.js"
+  },
+  {
+    "revision": "4f8261a136f46d9910270dca62033f34",
+    "url": "/portfolio/static/media/9e8a5c7b-b389-4f4d-a58c-55160fbd0878.4f8261a1.woff2"
+  },
+  {
+    "revision": "f621946285f3f5f1f80b6309cfac3831",
+    "url": "/portfolio/static/media/budget-tracker-screenshot.f6219462.png"
+  },
+  {
+    "revision": "e14e15b34cff51f9ab0334de13ee10a1",
+    "url": "/portfolio/static/media/chrome.e14e15b3.svg"
+  },
+  {
+    "revision": "728a078159dea7f2c15c20a5faa73891",
+    "url": "/portfolio/static/media/github.728a0781.svg"
+  },
+  {
+    "revision": "622e8904fb4bd2ec04253354bf356d5a",
+    "url": "/portfolio/static/media/horizontal-sign.622e8904.jpg"
+  },
+  {
+    "revision": "7c343f3120fa6fe44e7b8c9f11027715",
+    "url": "/portfolio/static/media/japan-alley.7c343f31.jpg"
+  },
+  {
+    "revision": "582fb3f85a006072ffc0d1dd36bd3342",
+    "url": "/portfolio/static/media/japan-gold.582fb3f8.jpg"
+  },
+  {
+    "revision": "d9049f461eb3dd661b7f77c6e56e7593",
+    "url": "/portfolio/static/media/linkedin.d9049f46.svg"
+  },
+  {
+    "revision": "c55cf3f9dac522cea5ff1374f136cbea",
+    "url": "/portfolio/static/media/media4u-index.c55cf3f9.png"
+  },
+  {
+    "revision": "fd3f02c589193ade60dd675e56655289",
+    "url": "/portfolio/static/media/portfolio-page.fd3f02c5.png"
+  },
+  {
+    "revision": "66dbf5a93c7c2d7752bbac54963f426c",
+    "url": "/portfolio/static/media/retro-japan.66dbf5a9.jpg"
+  },
+  {
+    "revision": "c470d55c8e3e1c624766695f821e4997",
+    "url": "/portfolio/static/media/run-buddy.c470d55c.png"
+  },
+  {
+    "revision": "1fc2f778722bdd89a45c4a35fafcd39d",
+    "url": "/portfolio/static/media/spotify.1fc2f778.svg"
+  },
+  {
+    "revision": "3634871266ed192c80ca557de481d70f",
+    "url": "/portfolio/static/media/tay.36348712.jpg"
+  },
+  {
+    "revision": "85815985339979a434d56960b2925fe2",
+    "url": "/portfolio/static/media/vertical-neon.85815985.jpg"
+  }
+]);
